30ccaf79e1a01f5dd27f8e1ca9962241d55b35ad
bosen L <bosenli2013@hotmail.com>
2022-03-01 22:18:51 -0500

    added removed node to linked list

--- linkedList.py
+++ linkedList.py
@@ -21,15 +21,41 @@ class Node:
 class LinkedList:
   def __init__(self, value=None):
     self.head_node = Node(value)
+
   def get_head_node(self):
     return self.head_node
+
   def insert_beginning(self,new_value ): #insert to beginning
     new_node = Node(new_value)
     new_node.set_next_node(self.head_node)
     self.head_node = new_node
-  def stringify_list(self)
-    stringlist = ""
+
+  def stringify_list(self):
+    string_list = ""
     current_node = self.get_head_node()
     while (current_node):
-        if (current_node.get_next_node() != None):
-            stringlist += str()
+        if (current_node.get_value() != None):
+            stringlist += str(current_node.get_value()) + "\n"
+        current_node = current_node.get_next_node()
+    return string_list
+
+ 
+  # Define your remove_node method below:
+  # def remove_node(self, value_to_remove):
+  #   current_node = self.head_node
+  #   if (current_node.get_value() == value_to_remove):
+  #     self.head_node = self.next_node
+  #   current_node = self.next_node
+
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()  #if it is current node need to be removed, then auto discard current node 
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()  
+        if next_node.get_value() == value_to_remove: # if it is next node, next remove next_node
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None #set none to current_node means to stop the while loop
+        else:
+          current_node = next_node
